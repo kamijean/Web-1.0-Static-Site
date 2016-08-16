8c4471eba9b1f3e49592b5207f8e27d1727b22f7
Bowen Masco <bowen@codingfoo.com>
2016-08-16 09:44:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# USU CS2610
+
+Your assignment is to build a static site that shows at least one
+"Today I Learned" entry in a separate page. There should be at least two different pages. The index page, and a page for at least  one Today I Learned entry. If you have more than one entry then they should be in separate pages
+
+The index page should have:
+* a nav element that should have a item that is a link to the index page. This same nav should be on both pages. The nav bar will have more links in the future.
+* a unordered list of the titles of the entries, that link to their respective pages.
+
+A Today I Learned entry page have:
+* The entries Title wrapped in an h1 tag.
+* The entries content wrapped in a p tag.
+* The entries creation date wrapped in a p tag.
+
+Three requirements for this assignment.
+
+1. Passes tests and code is avail on github.
+2. Deployed and available somewhere, the url will be submitted through canvas.
+3. Passes a human inspection of the site.
---
+++ spec/spec.js
@@ -0,0 +1,70 @@
+var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
+var w3cjs = require('w3cjs');
+
+describe("CS2610 Project Part1 index.html", function() {
+  it("should have a working test environment", function() {
+    expect(true).toBe(true);
+  });
+
+	it('should have an index page with no html errors', function(done){
+		w3cjs.validate({
+			file: 'index.html',
+			callback: function (res) {
+				console.log(res);
+				if (res.messages.length > 0 ) {
+					throw {error: 'html errors have been found', results: res};
+				};
+				done();
+			}
+		})
+	})
+
+  var indexFilePath = path.join(__dirname, '../index.html')
+  var indexFileContents = fs.readFileSync(indexFilePath, { encoding: 'utf8' });
+  var $ = cheerio.load(indexFileContents);
+
+  it('should have an index page with a link to index.html and at least one entry', function(){
+    expect($("a").length).toBeGreaterThan(1);
+	});
+
+  it('should have an index page with a list of entries', function(){
+    expect($("ul").length).toBeGreaterThan(0);
+  });
+
+  it('should have an index page with a nav section', function(){
+    expect($("nav").length).toBeGreaterThan(0);
+  });
+});
+
+describe("CS2610 Project Part1 entries/entry1.html", function() {
+	it('should have an entry1 page with no html errors', function(done){
+		w3cjs.validate({
+			file: 'entries/entry1.html',
+			callback: function (res) {
+				console.log(res);
+				if (res.messages.length > 0 ) {
+					throw {error: 'html errors have been found', results: res};
+				};
+				done();
+			}
+		})
+	})
+
+  var indexFilePath = path.join(__dirname, '../entries/entry1.html')
+  var indexFileContents = fs.readFileSync(indexFilePath, { encoding: 'utf8' });
+  var $ = cheerio.load(indexFileContents);
+
+  it('should have an entry page with a nav section', function(){
+    expect($("nav").length).toBeGreaterThan(0);
+  });
+
+  it('should have an entry page with a h1', function(){
+    expect($("h1").length).toBeGreaterThan(0);
+	});
+
+  it('should have an entry page with two p tags', function(){
+    expect($("p").length).toBeGreaterThan(1);
+  });
+});
